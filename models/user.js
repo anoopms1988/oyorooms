c03969536e2f431108f55942923623295da0bc5f
anoopms1988 <1988anoopms@gmail.com>
2017-11-21 05:03:24 +0530

    user model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var Schema=mongoose.Schema
+
+var UserSchema =new Schema(
+    {
+        firstname={type:String,required:true},
+        lastname={type:String,required:true},
+        email={type:String,required:true},
+        username={type:String,required:true},
+        password: { type: String, select: false },
+        active: { type: Boolean, default: false }
+    }
+);
+
+UserSchema.virtual('fullname').get(function(){
+    return firstname+' '+lastname;
+});
+
+//Export model
+module.exports = mongoose.model('user', UserSchema)
