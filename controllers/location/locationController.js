a8d9d1ba9b0dc48135cd39ad7584eb03d77a87c8
anoopms1988 <1988anoopms@gmail.com>
2017-12-11 05:24:22 +0530

    Create country

--- controllers/location/locationController.js
+++ controllers/location/locationController.js
@@ -1,6 +1,27 @@
 var LocationModels = require('../../models/location/location');
 var async = require('async');
 
-exports.create_location=function(req, res, next){
+exports.create_country=function(req, res, next){
+    req.checkBody('name', 'Country name must not be empty.').notEmpty();
+
+    var country=new LocationModels.Country({
+        name:req.body.name,
+        code:req.body.code
+    }
+    );
+
+    var errors = req.validationErrors();
+    if (errors) {
+        console.log(errors);
+    } else {
+        country.save(function (err) {
+            if (err) {
+                handleError(res, err);
+            } else {
+                res.send(country);
+            }
+            console.log(country);
+        });
+    }
     
 }
