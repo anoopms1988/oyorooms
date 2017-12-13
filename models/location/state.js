29ced5756d182f9e28351b6dd5b2f03c88989bb1
Anoop M S <1988anoopms@gmail.com>
2017-12-13 16:06:08 +0530

    Country details

--- models/location/state.js
+++ models/location/state.js
@@ -1,12 +1,16 @@
 var mongoose = require('mongoose');
 var Schema=mongoose.Schema
 
+
+
+
+
 var StateSchema=Schema(
     {
         name:{type:String,required:true},
         code :{type:String},
         status: { type: Boolean, default:true },
-        country: { type: Schema.ObjectId, ref: 'Country', required: true },
+        country: { type: Schema.ObjectId, ref: 'Country', required: true }
     }
 )
 
